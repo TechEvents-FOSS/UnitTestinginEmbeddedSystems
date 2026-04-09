3cd4409f87000a11667827c6c4b64a18478ba37c
Bharath G <bharathgopal75@gmail.com>
2026-04-09 14:18:13 +0000

    Added dependency installer for renode coverage

--- Lab3/install_tools.sh
+++ Lab3/install_tools.sh
@@ -5,3 +5,5 @@ sudo apt-get install -y gcc-arm-none-eabi gdb-multiarch binutils-arm-none-eabi
 wget https://github.com/renode/renode/releases/download/v1.16.1/renode_1.16.1_amd64.deb
 sudo dpkg -i renode_1.16.1_amd64.deb
 rm renode_1.16.1_amd64.deb
+
+python3 -m pip install pyelftools
